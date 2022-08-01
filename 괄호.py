29e654859e4f2f64cc0c0cb5e6d66a43034006b8
kimsehee827 <1816950@sookmyung.ac.kr>
2022-08-01 22:46:22 +0900

    괄호 배열문제

---
+++ 괄호.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    stk = []
+    ans = 'YES'
+    for c in input():
+        if c == '(':
+            stk.append(c)
+        else:
+            if len(stk) > 0:
+                stk.pop()
+            else:
+                ans='NO'
+
+    if len(stk) >0:
+        ans = 'NO'
+    print(ans)
